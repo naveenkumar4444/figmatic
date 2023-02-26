0bb02f05396a1735549fcac60fc08357496b1d82
Naveen Kumar <naveenkumar00004444@gmail.com>
2023-02-27 00:51:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,190 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import CounterButton from "./components/CounterButton";
 
 function App() {
+  const icon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      class="bi bi-info-circle"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+      <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+    </svg>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="table-responsive">
+        <table
+          className="table table-borderless"
+          // className="table"
+          style={{
+            height: "90vh",
+            overflow: "scroll",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <thead>
+            <tr>
+              <th
+                style={{
+                  fontSize: "65px",
+                }}
+              >
+                5
+                <sub
+                  style={{
+                    fontSize: "13px",
+                  }}
+                >
+                  Pages
+                </sub>
+              </th>
+              <td>
+                <span>$300</span>
+                <br />
+                <span>Page Design</span>
+              </td>
+              <td>
+                <span>$200</span>
+                <br />
+                <span>Interactions & Animations</span>
+              </td>
+              <td>
+                <span>$200</span>
+                <br />
+                <span>CMS / Dynamic Data / Intagrations</span>
+              </td>
+              <th>
+                <span>Sub Total</span>
+                <br />
+                <span
+                  style={{
+                    fontSize: "50px",
+                  }}
+                >
+                  $800
+                </span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Pages {icon}</th>
+              <th>Design {icon}</th>
+              <th>Interactions & Animations {icon}</th>
+              <th>Interaction {icon}</th>
+              <th
+                style={{
+                  textAlign: "right",
+                }}
+              >
+                Cost {icon}
+              </th>
+            </tr>
+            <tr>
+              <th>01. Home</th>
+              <td>
+                <CounterButton />
+              </td>
+              <td>
+                <CounterButton />
+              </td>
+              <td>
+                <CounterButton />
+              </td>
+              <th
+                style={{
+                  textAlign: "right",
+                }}
+              >
+                $200
+              </th>
+            </tr>
+            <tr>
+              <th>02. Service</th>
+              <td>
+                <CounterButton />
+              </td>
+              <td>
+                <CounterButton />
+              </td>
+              <td>
+                <CounterButton />
+              </td>
+              <th
+                style={{
+                  textAlign: "right",
+                }}
+              >
+                $200
+              </th>
+            </tr>
+            <tr>
+              <th>03. About Us</th>
+              <td>
+                <CounterButton />
+              </td>
+              <td>
+                <CounterButton />
+              </td>
+              <td>
+                <CounterButton />
+              </td>
+              <th
+                style={{
+                  textAlign: "right",
+                }}
+              >
+                $200
+              </th>
+            </tr>
+            <tr>
+              <th>04. Partners</th>
+              <td>
+                <CounterButton />
+              </td>
+              <td>
+                <CounterButton />
+              </td>
+              <td>
+                <CounterButton />
+              </td>
+              <th
+                style={{
+                  textAlign: "right",
+                }}
+              >
+                $200
+              </th>
+            </tr>
+            <tr>
+              <th>05. Contact Us</th>
+              <td>
+                <CounterButton />
+              </td>
+              <td>
+                <CounterButton />
+              </td>
+              <td>
+                <CounterButton />
+              </td>
+              <th
+                style={{
+                  textAlign: "right",
+                }}
+              >
+                $200
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/components/CounterButton.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+
+const CounterButton = ({}) => {
+  const values = ["None", "Basic", "Advanced", "Complex"];
+
+  const [name, setName] = useState(values[0]);
+
+  const handleIncrement = () => {
+    let i = values.indexOf(name);
+    const j = i + 1;
+    if (i !== 3) {
+      setName(values[j]);
+    }
+  };
+
+  const handleDecrement = () => {
+    let i = values.indexOf(name);
+    const j = i - 1;
+    if (i !== 0) {
+      setName(values[j]);
+    }
+  };
+
+  const getBackground = () => {
+    if (name == "None") {
+      return "lightgray";
+    }
+    if (name == "Basic") {
+      return "#91D4A8";
+    }
+    if (name == "Advanced") {
+      return "#E9BE74";
+    }
+    if (name == "Complex") {
+      return "#F57F6C";
+    }
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          //   width: "180px",
+          display: "inline-flex",
+          justifyContent: "center",
+          alignItems: "center",
+          //   alignContent: "baseline",
+          borderRadius: "100px",
+          background: "white",
+          height: "35px",
+        }}
+      >
+        <button
+          onClick={handleDecrement}
+          style={{
+            border: "none",
+            borderTopLeftRadius: "20px",
+            borderBottomLeftRadius: "20px",
+            padding: "0 10px",
+          }}
+          className="bg-white"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-dash"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z" />
+          </svg>
+        </button>
+        <span
+          style={{
+            // background: "#F57F6C",
+            background: getBackground(),
+            width: "200px",
+            textAlign: "center",
+            height: "100%",
+            paddingTop: "5px",
+            borderRadius: "50px",
+            // fontWeight: "bold",
+          }}
+        >
+          {name}
+        </span>
+        <button
+          onClick={handleIncrement}
+          style={{
+            border: "none",
+            borderTopRightRadius: "20px",
+            borderBottomRightRadius: "20px",
+            padding: "0 10px",
+          }}
+          className="bg-white"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-plus-lg"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CounterButton;
